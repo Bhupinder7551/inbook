2f482ef869d35aa4ef33e8edb00d8ad85c031439
Bhupinder Singh Sandhu <bhupindersingh7551@gmail.com>
2022-05-17 16:24:01 +1200

    step 3  pass data in mongodb

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,13 @@ const port = 5000
 app.get('/', (req, res) => {
 res.send ('Hello World!')
 })
+app.use(express.json())
+
+//Available routes
+
+app.use('/api/auth', require ('./routes/auth'))
+// app.use('/api/notes', require ('./routes/notes'))
+
 app.listen(port, () => {
 console.log( `Example app listening at http://localhost:${port}`)
 })
---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const {Schema}= mongoose;
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Password: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model('user', UserSchema);

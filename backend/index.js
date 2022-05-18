656f989f570f83c4dac589866a5ff24ee7ff20b0
Bhupinder Singh Sandhu <bhupindersingh7551@gmail.com>
2022-05-18 18:51:13 +1200

    crud and user login credentials

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 //Available routes
 
 app.use('/api/auth', require ('./routes/auth'))
-// app.use('/api/notes', require ('./routes/notes'))
+app.use('/api/notes', require ('./routes/notes'))
 
 app.listen(port, () => {
 console.log( `Example app listening at http://localhost:${port}`)
